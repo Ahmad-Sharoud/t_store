bf2ce32c30bd28532576fec8b26280577f81f393
Ahmad-Sharoud <a.sharoud@gmail.com>
2024-06-27 11:01:51 +0300

    this first commit

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,18 @@
+class TImages{
+
+  //App Logos
+  static const String lightAppLogo = "assets/logos/khdewi_logo_light.png";
+  static const String darkAppLogo = "assets/logos/khdewi_logo_dark.png";
+
+  //Social Logos
+  static const String google = "assets/icons/google_icon.png";
+  static const String facebook = "assets/icons/facebook-icon.png";
+
+  //Onboarding Images
+static const String onBoardingImage1 = "assets/images/on_boarding_images/sammy-line-searching.gif";
+static const String onBoardingImage2 = "assets/images/on_boarding_images/sammy-line-shopping.gif";
+static const String onBoardingImage3 = "assets/images/on_boarding_images/sammy-line-delivering.gif";
+
+
+
+}
