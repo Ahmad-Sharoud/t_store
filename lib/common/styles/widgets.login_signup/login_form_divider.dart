4a8a0a1c246df16810fba0d83a56395ea88170e8
Ahmad-Sharoud <a.sharoud@gmail.com>
2024-06-30 01:11:58 +0300

    home pc chaning

--- lib/common/styles/widgets.login_signup/login_form_divider.dart
+++ lib/common/styles/widgets.login_signup/login_form_divider.dart
@@ -17,28 +17,19 @@ class TFormDivider extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Flexible(
-          child: Divider(
-            color: dark ? TColors.softGrey : TColors.darkGrey,
-            thickness: 0.5,
-            indent: 60,
-            endIndent: 5,
-          ),
-        ),
-        Text(dividerText,
-            style: TextStyle(
-                color: dark
-                    ? TColors.softGrey
-                    : TColors
-                        .darkGrey) //Theme.of(context).textTheme.labelMedium,
-            ),
+            child: Divider(
+                color: dark ? TColors.grey : TColors.darkGrey,
+                thickness: 0.5,
+                indent: 60,
+                endIndent: 5)),
+        Text(dividerText, style: Theme.of(context).textTheme.labelMedium),
         Flexible(
-          child: Divider(
-            color: dark ? TColors.softGrey : TColors.darkGrey,
-            thickness: 0.5,
-            indent: 5,
-            endIndent: 60,
-          ),
-        ),
+            child: Divider(
+          color: dark ? TColors.grey : TColors.darkGrey,
+          thickness: 0.5,
+          indent: 5,
+          endIndent: 60,
+        )),
       ],
     );
   }
