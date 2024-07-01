cc5e9d96f12780abe73dee6d5b177e75d84fdced
Ahmad-Sharoud <a.sharoud@gmail.com>
2024-07-01 11:27:26 +0300

    Create Custom AppBar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/styles/widgets/appbar/appbar.dart';
+import 'package:t_store/common/styles/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +11,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// Header -- Tutorial [Section # 3, Video # 2]
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  TAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: TColors.grey)),
+                        Text(TTexts.homeAppbarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
